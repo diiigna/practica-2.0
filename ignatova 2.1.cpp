c10b073f45a88040cfce95140f64ef3ef5f006c8
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-20 09:57:19 +0300

    Практика 2.1

---
+++ ignatova 2.1.cpp
@@ -0,0 +1,24 @@
+﻿ 
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int num1;
+	int num2;
+	int result;
+	cout << "Ведите первое число:";
+	cin >> num1;
+	cout << "Введите второе число:";
+	cin >> num2;
+	cout << "Какой результат умножения этих 2х чискел?";
+	cin >> result;
+	if (result == num1 * num2) {
+		cout << "ура, верно!!!";
+	}
+	else {
+		cout << "Фу, неверно!!!";
+	}
+	return 0;
+}
+
