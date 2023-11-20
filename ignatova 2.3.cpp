5cfdf865e4f8f3db61e3395ed6310789df1c35cf
Диана <151121497+diiigna@users.noreply.github.com>
2023-11-20 10:42:39 +0300

    Практика 2.3

---
+++ ignatova 2.3.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int mas[7];
+	mas[1] = 1;
+	mas[2] = 2;
+	mas[3] = 3;
+	mas[4] = 4;
+	mas[5] = 5;
+	mas[6] = 6; 
+	int sum = 0;
+	for (int i = 0; i <= 6; i++) {
+		if (i % 2 != 0) {
+			sum += i;
+		}
+	}
+	cout << "Сумма нечетных чисел:" << sum << endl;
+}
+
